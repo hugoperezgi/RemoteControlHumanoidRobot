7f18b606f2ecb2709ba44f34eed2c86510fd7c3e
hugop <h.perez10@usp.ceu.es>
2024-09-11 12:22:41 +0200

    mcuCode ini

---
+++ codeMCU/robotControl_ESP32/secrets.h
@@ -0,0 +1,6 @@
+/* Wi-Fi name/password */
+#define WIFI_SSID     "YOUR-WIFI-NAME"
+#define WIFI_PASSWORD "YOUR-WIFI-PASSWORD"
+/* Firebase Setup */
+#define REFERENCE_URL "https://your-database.firebaseio.com/"
+#define AUTH_TOKEN "YOUR-AUTHENTICATION-CODE"
