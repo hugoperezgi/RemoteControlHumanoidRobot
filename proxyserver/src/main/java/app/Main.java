7c821f145818d5d372b212b14b512d10c4f35c91
hugop <h.perez10@usp.ceu.es>
2024-09-25 14:47:09 +0200

    Client Handler wip

--- proxyserver/src/main/java/app/Main.java
+++ proxyserver/src/main/java/app/Main.java
@@ -1,18 +1,43 @@
 package app;
 
-import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import controllers.ClientHandler;
 import controllers.FirebaseController;
 
 public class Main {
-    public static void main(String[] args) throws IOException, InterruptedException {      
-        System.out.println("FUKC!");  
-        FirebaseController f = new FirebaseController();
-        System.out.println("FUKC!");  
-        f.ini();
-        System.out.println("FUKC!");  
-        System.out.println(f.setCurrentPos(0,139));
-        System.out.println(f.getCurrentPos(0));
-        System.out.println("FUKC!");  
-        f.close();
+    public static void main(String[] args) throws Exception {   
+        ServerSocket s = new ServerSocket();
+        InetSocketAddress serverIPAddr = new InetSocketAddress("127.0.0.1",50500); 
+        s.bind(serverIPAddr); 
+        while (true){
+            try {
+                ClientHandler clientThread = new ClientHandler(s.accept());
+                clientThread.setDaemon(true);
+                clientThread.start();
+            } catch (Exception e) {
+                System.err.println(".accept() Error");
+                e.printStackTrace();
+            }
+        }
+        // fb();
     }
+
+
+    // private static void fb()throws Exception{
+    //     FirebaseController f = new FirebaseController();
+    //     f.ini();
+    //     System.out.println(f.getCurrentPos(0));
+    //     System.out.println(f.setCurrentPos(0,139));
+    //     System.out.println(f.getCurrentPos(0));
+    //     System.out.println(f.setCurrentPos(0,178));
+    //     System.out.println(f.setCurrentPos(0,187));
+    //     System.out.println(f.getCurrentPos(0));
+    //     System.out.println(f.setCurrentPos(0,101));
+    //     System.out.println(f.getCurrentPos(0));
+    //     System.out.println(f.setCurrentPos(0,27));
+    //     System.out.println(f.getCurrentPos(0));
+    //     f.setUpdatePostion();
+    //     f.close();
+    // }
 }
--- proxyserver/src/main/java/controllers/ClientHandler.java
+++ proxyserver/src/main/java/controllers/ClientHandler.java
@@ -1,20 +1,44 @@
 package controllers;
 
+import java.io.IOException;
+import java.net.Socket;
+
 public class ClientHandler extends Thread{
     
     boolean run = true;
+    private Socket sck;
+    byte[] byteBuff = new byte[255];
 
     @Override
     public void run() {
         run=true;
 
         while(run){
+            try {
+                
+                //Get the query
+                sck.getInputStream().read(byteBuff);
+                String s="";
+                for (byte b : byteBuff) {
+                    if(b==0){break;}
+                    s+=(char)b;
+                }
 
+                //Check query format
+                /* TODO */
+
+                //Switch query
+                /* TODO */
+                
+            } catch (IOException e) {
+                run=false;
+            }
         }
         
     }
 
-    public ClientHandler(){
+    public ClientHandler(Socket s){
+        this.sck=s;
     }
 
 }
