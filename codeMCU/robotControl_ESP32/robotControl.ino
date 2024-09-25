85e6d848a303a76284c0f4f45600d0cba798385d
hugop <h.perez10@usp.ceu.es>
2024-09-25 14:53:44 +0200

    setup ready

--- codeMCU/robotControl_ESP32/robotControl.ino
+++ codeMCU/robotControl_ESP32/robotControl.ino
@@ -1,6 +1,35 @@
-void setup() {
-  // put your setup code here, to run once:
+#include "secrets.h"
+#include "servoCalibration.h"
+// #include <WiFi.h>
+#include <Firebase.h>
+  Firebase fb(REFERENCE_URL, AUTH_TOKEN);
 
+#include <Adafruit_PWMServoDriver.h>
+  Adafruit_PWMServoDriver pwm0 = Adafruit_PWMServoDriver(0x40);
+  Adafruit_PWMServoDriver pwm1 = Adafruit_PWMServoDriver(0x41);
+
+
+void setup() {
+  Serial.begin(9600);
+  // WiFi.disconnect();
+  // delay(1000);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(WIFI_SSID, WIFI_PSSW);
+  // WiFi.setSleep(false);
+  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    Serial.println(WiFi.waitForConnectResult());
+    // WiFi.reconnect();
+    delay(1000);
+  }
+
+  pwm0.begin();
+  pwm0.setOscillatorFrequency(27000000);
+  pwm0.setPWMFreq(SERVO_FREQ); 
+
+  pwm1.begin();
+  pwm1.setOscillatorFrequency(27000000);
+  pwm1.setPWMFreq(SERVO_FREQ); 
+  // fb.setInt("currentPosition/servo0",29);
 }
 
 void loop() {
---
+++ codeMCU/robotControl_ESP32/servoCalibration.h
@@ -0,0 +1,1 @@
+#define SERVO_FREQ 50 // Analog servos run at ~50 Hz updates
