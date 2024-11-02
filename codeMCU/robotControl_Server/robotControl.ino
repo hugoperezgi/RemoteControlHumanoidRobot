4a152900448a6c2970f107f1fa974cebaa1974ca
hugop <h.perez10@usp.ceu.es>
2024-11-02 02:40:42 +0100

    WiFi client on ESP32 setup

--- codeMCU/robotControl_Server/robotControl.ino
+++ codeMCU/robotControl_Server/robotControl.ino
@@ -1,3 +1,22 @@
+#include "secrets.h"
+#include "servoCalibration.h"
+
+#include <Arduino.h>
+#include <stdint.h>
+
+#include <WiFi.h>
+#include <WiFiClientSecure.h>
+// #include <WiFiClient.h>
+
+#include <Adafruit_PWMServoDriver.h>
+
+//Server ip:port
+const uint16_t port = SERVER_PORT;
+const char *host = SERVER_IP;
+//Wifi data
+const char *ssid = WIFI_SSID;  
+const char *password = WIFI_PASSWORD;
+
 void setup() {
   // put your setup code here, to run once:
 
