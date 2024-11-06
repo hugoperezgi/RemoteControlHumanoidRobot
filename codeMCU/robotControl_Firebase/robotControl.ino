2547709254bca8f76935de56df0f20377ff51452
hugop <h.perez10@usp.ceu.es>
2024-11-06 16:10:34 +0100

    Firebase solution properly implemented/finished

--- codeMCU/robotControl_Firebase/robotControl.ino
+++ codeMCU/robotControl_Firebase/robotControl.ino
@@ -7,7 +7,7 @@
 #include <Adafruit_PWMServoDriver.h>
   Adafruit_PWMServoDriver pwm0 = Adafruit_PWMServoDriver(0x40);
   Adafruit_PWMServoDriver pwm1 = Adafruit_PWMServoDriver(0x41);
-//   Adafruit_PWMServoDriver pwm2 = Adafruit_PWMServoDriver(0x42);
+  Adafruit_PWMServoDriver pwm2 = Adafruit_PWMServoDriver(0x42);
 
 uint8_t currentPostition[27],targetPosition[27];
 uint8_t i,j;
@@ -35,9 +35,9 @@ void setup() {
   pwm1.setOscillatorFrequency(27000000);
   pwm1.setPWMFreq(SERVO_FREQ); 
 
-//   pwm2.begin();
-//   pwm2.setOscillatorFrequency(27000000);
-//   pwm2.setPWMFreq(SERVO_FREQ); 
+  pwm2.begin();
+  pwm2.setOscillatorFrequency(27000000);
+  pwm2.setPWMFreq(SERVO_FREQ); 
 }
 
 void loop() {
@@ -62,22 +62,21 @@ void loop() {
 
         for (i=0; i < 27; i++){ 
             if((cc & (0b1<<i))==(0b1<<i)){
-				if(i<6){/* LeftHand Servo Controls */
+				if(i<6){/* LeftHand Servo Controls (pwm board 0) - Servos 0-5 (6) */
 					pwm0.setPWM(i,0,minServoPos[i]+targetPosition[i]*(maxServoPos[i]-minServoPos[i])/180);delay(10);
-				}else if(i<12){ /* LeftHand Servo Controls */
+				}else if(i<12){ /* RightHand Servo Controls (pwm board 2) - Servos 6-11 (6) */
 					j=i-6;
-					//TODO - RightHand servo control (pwm board 2) - Servos 6-11 (6)
-					// pwm2.setPWM(j,0,minServoPos[i]+targetPosition[i]*(maxServoPos[i]-minServoPos[i])/180);delay(10);
-				}else{ /* Body/Head Servo Controls */
+					pwm2.setPWM(j,0,minServoPos[i]+targetPosition[i]*(maxServoPos[i]-minServoPos[i])/180);delay(10);
+				}else{ /* Body/Head Servo Controls (pwm board 1) - Servos 12-26 (15) */
 					j=i-12;
-					//TODO - Body servo control (pwm board 1) - Servos 12-26 (15)
-					// pwm1.setPWM(j,0,minServoPos[i]+targetPosition[i]*(maxServoPos[i]-minServoPos[i])/180);delay(10);
+					pwm1.setPWM(j,0,minServoPos[i]+targetPosition[i]*(maxServoPos[i]-minServoPos[i])/180);delay(10);
 				}
             }
         }
 
 		fb.setBool("FlagMoveServo",false);
 		fb.setInt("ServoUpdateFlag",0);
+        cc=0;
 
 	}
     delay(1000);
