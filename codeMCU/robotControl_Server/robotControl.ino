c063bf8f1e422e2b51f3e1f9dc2eafd9d693e10f
hugop <h.perez10@usp.ceu.es>
2024-12-09 19:59:19 +0100

    MCU server connection working TODO implement logic

--- codeMCU/robotControl_Server/robotControl.ino
+++ codeMCU/robotControl_Server/robotControl.ino
@@ -1,5 +1,5 @@
 #include "secrets.h"
-#include "servoCalibration.h"
+// #include "servoCalibration.h"
 
 #include <Arduino.h>
 #include <stdint.h>
@@ -8,21 +8,81 @@
 #include <WiFiClientSecure.h>
 // #include <WiFiClient.h>
 
-#include <Adafruit_PWMServoDriver.h>
+// #include <Adafruit_PWMServoDriver.h>
 
-//Server ip:port
-const uint16_t port = SERVER_PORT;
-const char *host = SERVER_IP;
-//Wifi data
-const char *ssid = WIFI_SSID;  
-const char *password = WIFI_PASSWORD;
+WiFiClient client;
+String query = "";
 
-void setup() {
-  // put your setup code here, to run once:
+void wrongFormat(){
+  client.print("NACK");
+}
+
+void handleQuery(){
+  switch(query.charAt(1)){
+    case 'm': /* Move servos */
+      Serial.println("Move Servos");
+      client.print("ACK");
+      break;
+    case 'u': /* Update target positions (For delayed mode) */
+      Serial.println("Update positions");
+      client.print("ACK");
+      break;
+    case 'e': /* Execute all movements (Delayed mode) */
+      Serial.println("Execute movements");
+      client.print("ACK");
+      break;
+    default:
+      wrongFormat();
+      break;
+  }
 
 }
 
+void setup() {
+  Serial.begin(9600);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(WIFI_SSID, WIFI_PSSW);
+
+  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    Serial.print("Stuck at wifi connection: ");
+    Serial.println(WiFi.waitForConnectResult());
+    // WiFi.reconnect();
+    delay(1000);
+  }
+
+  // pwm0.begin();
+  // pwm0.setOscillatorFrequency(27000000);
+  // pwm0.setPWMFreq(SERVO_FREQ); 
+
+  // pwm1.begin();
+  // pwm1.setOscillatorFrequency(27000000);
+  // pwm1.setPWMFreq(SERVO_FREQ); 
+
+  // pwm2.begin();
+  // pwm2.setOscillatorFrequency(27000000);
+  // pwm2.setPWMFreq(SERVO_FREQ); 
+}
+
 void loop() {
-  // put your main code here, to run repeatedly:
+  /* Check server connection - reconnect if necessary */
+  if(!client.connected()){
+    if (!client.connect(SERVER_IP, SERVER_PORT)){
+      Serial.println("Connection to server failed");
+      delay(1000);
+      return;
+    }else{
+      /* Say hello to server and stop reseting the connection */
+      client.print("!s-NodeMCU_here-smartMCU-\6-\1-\1-\1-\1-\1-\1-e!");
+    }
+  }
+  /* Control loop */
+  query="";
+  if(client.available()){
+    query = client.readStringUntil('\n');
+    Serial.println(query);
+
+    (query.startsWith("-")&&query.endsWith("-!")) ? handleQuery() : wrongFormat();
+  }
 
 }
--- codeMCU/robotControl_Server/secrets.h
+++ codeMCU/robotControl_Server/secrets.h
@@ -1,6 +1,6 @@
 /* Wi-Fi name/password */
-#define WIFI_SSID     "YOUR-WIFI-NAME"
-#define WIFI_PASSWORD "YOUR-WIFI-PASSWORD"
+#define WIFI_SSID "YOUR-WIFI-NAME"
+#define WIFI_PSSW "YOUR-WIFI-PASSWORD"
 /* Server ip/port */  
 #define SERVER_PORT   0
 #define SERVER_IP     "172.16.0.0"
